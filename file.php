874125352e7492a925a7ada0fc5c5dfa3c262a0a
Pierre Charlier <pcepegra@gmail.com>
2021-03-01 09:02:43 +0100

    phpmailer add

--- file.php
+++ file.php
@@ -65,49 +65,19 @@ unlink("recipes.json");
 //print_r($json);
 
 //envoi par email
+require_once('phpmailer/PHPMmailer.php');
 
-// On va dabors définir le fichier à envoyer et à qui
-$fichier = "exports/".$slug.".rtk";
-$destinataire = 'lretlo@gmail.com';
-// On créer un boundary unique
-$boundary = md5(uniqid(rand(), true));
-// On met les entêtes
-$entetes = array(
-    'From' => 'webmaster@example.com',
-    'Content-Type' => 'multipart/mixed;',
-    'boundary' => $boundary
-);
+$email = new PHPMailer();
+$email->SetFrom('pcepegra@gmail.com', 'Pierre <3'); //Name is optional
+$email->Subject   = 'Recette tek';
+$email->Body      = "essai";
+$email->AddAddress( 'lretlo@gmail.com' );
 
-//$entetes = 'Content-Type: multipart/mixed;' . '\r\n' . ' boundary="'.$boundary.'"\r\n'.'From: lretlo@gmail.com' . '\r\n';
-$body = 'This is a multi-part message in MIME format.'."n";
-$body .= '--'.$boundary."n";
-// ici, c'est la première partie, notre texte HTML (ou pas !)
-// Là, on met l'entête
-$body .= 'Content-Type: text/html; charset="UTF-8"'."n";
-// On peut aussi mettres les autres entêtes (voir à la fin)
-$body .= "n";
-// On remet un deuxième retour à la ligne pour dire que les entêtes sont finie, on peut afficher notre texte !
-$body .= 'Bonjour,
-Voici ci-joint la facture de Juillet 2008 a payer sous 2 heures';
-// Le texte est finie, on va faire un saut à la ligne
-$body .= "n";
-// Et on commence notre deuxième partie qui va contenir le PDF
-$body .= '--'.$boundary."n";
-// On lui dit (dans le Content-type) que c'est un fichier PDF
-$body .= 'Content-Type: octet-stream; name="'.$fichier.'"'."n";
-$body .= 'Content-Transfer-Encoding: base64'."n";
-$body .= 'Content-Disposition: attachment; filename="'.$fichier.'"'."n";
-// Les entêtes sont finies, on met un deuxième retour à la ligne
-$body .= "n";
+$file_to_attach = "exports/".$slug.".rtk";
 
-//traitement du fichier
-$source = file_get_contents($fichier);
-$source = base64_encode($source);
-$source = chunk_split($source);
-// On ferme la dernière partie :
-$body .= "n".'--'.$boundary.'--';
-// On envoi le mail :
-mail($destinataire, $sujet, $body, $entetes);
+$email->AddAttachment( $file_to_attach , $slug.".rtk" );
+
+return $email->Send();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
---
+++ test-file.php
@@ -0,0 +1,4 @@
+<?php print_r($_FILES);?>
+<form action="" method="post" enctype="multipart/form-data">
+<input type="file" name="file">
+<button>envoi</button></form>
