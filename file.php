305c4942f335501ae71f1e0964bd1645d111fee0
Pierre Charlier <pcepegra@gmail.com>
2021-03-01 10:03:59 +0100

    include smtp

--- file.php
+++ file.php
@@ -78,7 +78,7 @@ unlink("recipes.json");
 
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
-//require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 try {
